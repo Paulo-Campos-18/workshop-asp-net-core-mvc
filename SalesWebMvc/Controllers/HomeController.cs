b2533a1950708c304bd4fee263bd499706da92b5
Paulo-Campos-18 <pamcampos05@gmail.com>
2025-12-04 16:04:19 -0400

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Index()
         {
+            ViewData["Title"] = "Meio doido esse trem";
             return View();
         }
 
