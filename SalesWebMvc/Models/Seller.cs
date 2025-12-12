2d572507201c78ced992cb4ef3830cb392766243
Paulo-Campos-18 <pamcampos05@gmail.com>
2025-12-12 17:38:06 -0400

    Asynchronous operations using Tasks (async, await)

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -23,7 +23,7 @@ namespace SalesWebMvc.Models
         [DisplayFormat(DataFormatString = "{0:dd//MM/yyyy}")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
-        public Department Department { get; set; }
+        public Department? Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
